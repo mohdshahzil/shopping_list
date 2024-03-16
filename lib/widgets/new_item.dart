085959f55a5d13d98d1a3fbf46127f5c84f8b1e9
mohdshahzil <theshahzil@gmail.com>
2024-03-17 02:40:22 +0530

    form aware dropdown

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -28,6 +29,42 @@ class _NewItemState extends State<NewItem> {
                   return 'Demo...';
                 },
               ), //instead of TextField
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final categoryy in categories.entries)
+                        DropdownMenuItem(
+                            value: categoryy.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: categoryy.value.color,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                Text(categoryy.value.title)
+                              ],
+                            ))
+                    ], onChanged: (value) {}),
+                  )
+                ],
+              )
             ],
           ),
         ),
